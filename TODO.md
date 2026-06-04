a4083b2000f4b20e9bceef44f7fe8a2202762a88
jade fisher <fisherrjd@gmail.com>
2026-06-04 16:38:51 -0600

    init

---
+++ TODO.md
@@ -0,0 +1,37 @@
+Create a chore tracker app for my household.
+
+I want a file that is easily loadable / changeable that maintains a list of all the chores / rooms we have in the house 
+The goal of this is a "daily" chore for X people that rotates so we have a clean house with every half cycle since we can split the chores X ways
+
+This should send an update to each persons ntfy server respectively and maybe have some web interface for updating chore defintions / household members! 
+
+Example 4 Chores 2 people total (this can expand to X chores with X People ): 
+ntfy.com/jade
+ntfy.com/hannah
+
+Day 1
+
+Hannah - Kitchen
+Jade - Master Bedroom
+
+Day 2 
+
+Hannah - Living Room
+Jade - Own Office
+
+Day 3
+
+Hannah - Master Bedroom
+Jade - Kitchen
+
+Day 4 
+
+Hannah - Own Office
+Jade - Living Room
+
+Tech Stack: 
+
+Backend:
+- Python
+- Fast API
+- uv2nix
---
+++ default.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import
+    (fetchTarball {
+      name = "jpetrucciani-2026-06-04";
+      url = "https://github.com/jpetrucciani/nix/archive/e5adfb44f73ac8a1e053c34330f642749f577e51.tar.gz";
+      sha256 = "03qylaa6sw1wn762g0w5zcfy27584hpdk1fc91yisrbm2vcif79v";
+    })
+    { }
+}:
+let
+  name = "vibe";
+  uvEnv = pkgs.uv-nix.mkEnv {
+    inherit name; python = pkgs.python314;
+    workspaceRoot = pkgs.hax.filterSrc { path = ./.; };
+    pyprojectOverrides = final: prev: { };
+  };
+
+  tools = with pkgs; {
+    cli = [
+      jfmt
+      nixup
+    ];
+    uv = [ uv uvEnv ];
+    scripts = pkgs.lib.attrsets.attrValues scripts;
+  };
+
+  scripts = with pkgs; { };
+  paths = pkgs.lib.flatten [ (builtins.attrValues tools) ];
+  env = pkgs.buildEnv {
+    inherit name paths; buildInputs = paths;
+  };
+in
+(env.overrideAttrs (_: {
+  inherit name;
+  NIXUP = "0.0.10";
+} // uvEnv.uvEnvVars)) // { inherit scripts; }
